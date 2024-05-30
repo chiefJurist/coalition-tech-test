213eccd6dced53b51a718c8e0ff302a19fd95058
chiefJurist <anthonynnanna004@gmail.com>
2024-05-30 19:37:42 +0100

    anothe commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
-<script>
+<script setup>
 // eslint-disable-next-line no-unused-vars
 import { RouterView } from 'vue-router'
 import HeaderNav from './components/HeaderNav.vue'
-
-export default {
-    components: { HeaderNav }
-}
 </script>
 
 <template>
     <div>
         <HeaderNav />
-        <!-- <RouterView /> -->
+        <RouterView />
     </div>
 </template>
