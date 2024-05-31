fb37e8cadfcdb6b615e000b58ff492ffbb79cee8
chiefJurist <anthonynnanna004@gmail.com>
2024-05-31 06:11:53 +0100

    Another commit

--- src/views/PatientsView.vue
+++ src/views/PatientsView.vue
@@ -1,7 +1,31 @@
+<script>
+import { PatientsList } from '../components/PatientsList.vue'
+import { Diagnosis } from '../components/DiagnosisHistory.vue'
+import { DiagnosticList } from '../components/DiagnosticList.vue'
+import { Profile } from '../components/JessicaProfile.vue'
+import { Results } from '../components/LabResults.vue'
+
+export default {
+    components: { PatientsList, Diagnosis, DiagnosticList, Profile, Results }
+}
+</script>
 <template>
     <div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <!--Patients List-->
+        <div>
+            <PatientsList></PatientsList>
+        </div>
+
+        <!--Diagnosis and Diagnostic-->
+        <div>
+            <div><Diagnosis></Diagnosis></div>
+            <div><DiagnosticList></DiagnosticList></div>
+        </div>
+
+        <!--More Info on Patient-->
+        <div>
+            <div><Profile></Profile></div>
+            <div><Results></Results></div>
+        </div>
     </div>
 </template>
