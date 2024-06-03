3ad45c548a6d6e88df948cf7d6628432e777722d
chiefJurist <anthonynnanna004@gmail.com>
2024-06-03 19:48:40 +0100

    another commit

--- src/views/PatientsView.vue
+++ src/views/PatientsView.vue
@@ -11,14 +11,14 @@ export default {
 }
 </script>
 <template>
-    <div class="bg-custom-one py-10">
+    <div class="bg-custom-one py-10 grid grid-cols-4 gap-5 p-5">
         <!--Patients List-->
         <div>
             <PatientsList></PatientsList>
         </div>
 
         <!--Diagnosis and Diagnostic-->
-        <div>
+        <div class="col-span-2">
             <div><Diagnosis></Diagnosis></div>
             <div><DiagnosticList></DiagnosticList></div>
         </div>
