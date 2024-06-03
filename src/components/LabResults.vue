70846b2dca2b0fd4262989d9201e071e5e3697b3
chiefJurist <anthonynnanna004@gmail.com>
2024-06-03 05:13:21 +0100

    another commit

--- src/components/LabResults.vue
+++ src/components/LabResults.vue
@@ -1,3 +1,45 @@
 <template>
-    <div></div>
+    <div class="my-5 labres-gen-con">
+        <!--Lab Result Header-->
+        <div class="labres-header">Lab Results</div>
+
+        <!--Lab Result-->
+        <div class="relative">
+            <!-- Vertical Progress Bar -->
+            <div class="progress-bar-con">
+                <div class="progress-bar" style="height: 20%"></div>
+            </div>
+            <!--Lab Result Body-->
+            <div class="labres-con">
+                <span>Blood tests</span>
+                <span class="cursor-pointer">
+                    <img src="/public/img/download.png" alt="download" />
+                </span>
+            </div>
+            <div class="labres-con bg-custom-one">
+                <span>CT Scans</span>
+                <span class="cursor-pointer">
+                    <img src="/public/img/download.png" alt="download" />
+                </span>
+            </div>
+            <div class="labres-con">
+                <span>Radiology Reports</span>
+                <span class="cursor-pointer">
+                    <img src="/public/img/download.png" alt="download" />
+                </span>
+            </div>
+            <div class="labres-con">
+                <span>X-Rays</span>
+                <span class="cursor-pointer">
+                    <img src="/public/img/download.png" alt="download" />
+                </span>
+            </div>
+            <div class="labres-con">
+                <span>Urine Test</span>
+                <span class="cursor-pointer">
+                    <img src="/public/img/download.png" alt="download" />
+                </span>
+            </div>
+        </div>
+    </div>
 </template>
