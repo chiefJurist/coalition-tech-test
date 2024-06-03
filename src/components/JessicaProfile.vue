3bfc517e8c2e3a318304ca6867e5c46e394dfcab
chiefJurist <anthonynnanna004@gmail.com>
2024-06-03 04:40:08 +0100

    another commit

--- src/components/JessicaProfile.vue
+++ src/components/JessicaProfile.vue
@@ -1,3 +1,55 @@
 <template>
-    <div></div>
+    <div class="my-5 profile-gen-con">
+        <div>
+            <img src="/img/jessica-taylor.png" alt="jessica" />
+        </div>
+
+        <div class="profile-name">Jessica Taylor</div>
+
+        <div class="profile-info-con">
+            <div class="profile-info-marker">
+                <img src="/img/calender.png" alt="calendar" />
+            </div>
+            <div class="text-sm">
+                <div class="">Date Of Birth</div>
+                <div class="font-bold">August 23, 1996</div>
+            </div>
+        </div>
+        <div class="profile-info-con">
+            <div class="profile-info-marker">
+                <img src="/img/female-icon.png" alt="female" />
+            </div>
+            <div class="text-sm">
+                <div>Gender</div>
+                <div class="font-bold">Female</div>
+            </div>
+        </div>
+        <div class="profile-info-con">
+            <div class="profile-info-marker">
+                <img src="/img/phone-icon.png" alt="phone" />
+            </div>
+            <div class="text-sm">
+                <div>Contact Info</div>
+                <div class="font-bold">(415) 555-1234</div>
+            </div>
+        </div>
+        <div class="profile-info-con">
+            <div class="profile-info-marker">
+                <img src="/img/phone-icon.png" alt="phone" />
+            </div>
+            <div class="text-sm">
+                <div>Emergency Contacts</div>
+                <div class="font-bold">(415) 555-5678</div>
+            </div>
+        </div>
+        <div class="profile-info-con">
+            <div class="profile-info-marker">
+                <img src="/img/insurance-icon.png" alt="insurance" />
+            </div>
+            <div class="text-sm">
+                <div>Insurance Provider</div>
+                <div class="font-bold">Sunrise Health Assurance</div>
+            </div>
+        </div>
+    </div>
 </template>
